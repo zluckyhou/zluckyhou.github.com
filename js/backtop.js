c849023c3f8623dc37e5990f9d1d357b3f34a089
cnfeat <cnfeat@gmail.com>
2015-08-29 21:22:23 +0800

    $("#back-top").hide(); $(document).ready(function () {   $(window).scroll(function () {     if ($(this).scrollTop() > 100) {       $('#back-top').fadeIn();     } else {       $('#back-top').fadeOut();     }   });   $('#back-top a').click(function () {     $('body,html').animate({       scrollTop: 0     }, 800);     return false;   }); });

--- js/backtop.js
+++ js/backtop.js
@@ -1,13 +1,16 @@
-$(function(){
-    $(window).scroll(function(){  //只要窗口滚动,就触发下面代码 
-        var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度 
-        if( scrollt >200 ){  //判断滚动后高度超过200px,就显示  
-            $("#gotop").fadeIn(400); //淡出     
-        }else{      
-            $("#gotop").stop().fadeOut(400); //如果返回或者没有超过,就淡入.必须加上stop()停止之前动画,否则会出现闪动   
-        }
-    });
-    $("#gotop").click(function(){ //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
-            $("html,body").animate({scrollTop:"0px"},200);
-    });
+$("#back-top").hide();
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#back-top').fadeIn();
+    } else {
+      $('#back-top').fadeOut();
+    }
+  });
+  $('#back-top a').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
 });

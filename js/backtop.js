7a6cf3794f9d3bf6b871b699502f4f34ac27cebf
cnfeat <cnfeat@gmail.com>
2015-08-29 21:03:42 +0800

    totally change

--- js/backtop.js
+++ js/backtop.js
@@ -1,16 +1,36 @@
-$("#back-top").hide();
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 800) {
-      $('#back-top').fadeIn(1500);
-    } else {
-      $('#back-top').fadeOut(1000);
-    }
-  });
-  $('#back-top a').click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 2500);
-    return false;
-  });
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+
+<script>
+
+$(document).ready(function(){ // hide #back-top first
+
+$("#back-top").hide(); // fade in #back-top
+
+$(function () { $(window).scroll(function () {
+
+if ($(this).scrollTop() > 100) { $('#back-top').fadeIn();
+
+}
+
+else { $('#back-top').fadeOut(); } });
+
+// scroll body to 0px on click
+
+$('#back-top a').click(function () {
+
+$('body,html').animate({
+
+scrollTop: 0
+
+}, 800);
+
+return false;
+
 });
+
+});
+
+});
+
+</script>

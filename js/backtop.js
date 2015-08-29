ce628cc88382e63fd8351b61309751cdaa01dc5f
cnfeat <cnfeat@gmail.com>
2015-08-29 21:19:17 +0800

    ag2

--- js/backtop.js
+++ js/backtop.js
@@ -1,31 +1,13 @@
-$(document).ready(function() {
-
-    // hide #back-top first
-    $("#back-top").hide();
-
-    // fade in #back-top
-    $(function() {
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > 100) {
-                $('#back-top').fadeIn();
-            } else {
-                $('#back-top').fadeOut();
-            }
-        });
-
-        // scroll body to 0px on click
-        $('#back-top a').click(function() {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
+$(function(){
+    $(window).scroll(function(){  //只要窗口滚动,就触发下面代码 
+        var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度 
+        if( scrollt >200 ){  //判断滚动后高度超过200px,就显示  
+            $("#gotop").fadeIn(400); //淡出     
+        }else{      
+            $("#gotop").stop().fadeOut(400); //如果返回或者没有超过,就淡入.必须加上stop()停止之前动画,否则会出现闪动   
+        }
     });
-
+    $("#gotop").click(function(){ //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
+            $("html,body").animate({scrollTop:"0px"},200);
+    });
 });
-
-HTML USAGE:
-
-<p id="back-top" style="display: block; ">
-   <a href="#top"><span></span><em>Back to top</em></a>
-</p>
